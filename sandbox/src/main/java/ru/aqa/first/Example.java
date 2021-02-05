2040ace69038c8039fb95ac25750258c67195428
GeisAnn <78374113+GeisAnn@users.noreply.github.com>
2021-02-05 18:38:35 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/aqa/first/Example.java
+++ sandbox/src/main/java/ru/aqa/first/Example.java
@@ -3,7 +3,29 @@ package ru.aqa.first;
 public class Example {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		hello("world");
+		hello("Mark");
+		hello("Anna");
+
+		double l = 5;
+		System.out.println("Ploschad kvadrata so storonoi " + l + " = " + area(l));
+
+		double a = 4;
+		double b = 6;
+		System.out.println("Ploschad pryamougolnika so storonami " + a + " i " + b + " = " + area(a,b));
+		}
+
+
+	public static void hello(String somebody) {
+
+		System.out.println("Hello " + somebody + "!");
 	}
 
+	public static double area(double len) {
+	    return len *  len;
+	}
+
+	public static double area(double a, double b) {
+		return a * b;
+}
 }
