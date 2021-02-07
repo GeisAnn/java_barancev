65bb46074f38194f825756f7b715ac850b4349c9
GeisAnn <78374113+GeisAnn@users.noreply.github.com>
2021-02-07 15:01:09 +0300

    Переделывае функцию в метод

--- sandbox/src/main/java/ru/aqa/first/Example.java
+++ sandbox/src/main/java/ru/aqa/first/Example.java
@@ -9,11 +9,11 @@ public class Example {
 
 		Square s = new Square(5);
 
-		System.out.println("Ploschad kvadrata so storonoi " + s.l + " = " + area(s));
+		System.out.println("Ploschad kvadrata so storonoi " + s.l + " = " + s.area());
 
 		Rectangle r = new Rectangle(4, 6);
 
-		System.out.println("Ploschad pryamougolnika so storonami " + r.a + " i " + r.b + " = " + area(r));
+		System.out.println("Ploschad pryamougolnika so storonami " + r.a + " i " + r.b + " = " + r.area());
 		}
 
 
@@ -22,11 +22,7 @@ public class Example {
 		System.out.println("Hello " + somebody + "!");
 	}
 
-	public static double area(Square s) {
-	    return s.l *  s.l;
-	}
 
-	public static double area(Rectangle r) {
-		return r.a * r.b;
+
 }
 }
