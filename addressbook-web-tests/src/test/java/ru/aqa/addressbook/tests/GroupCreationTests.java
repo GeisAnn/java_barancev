78d14c7b6b5f3f436352902898600dff8ce0d0ed
GeisAnn <78374113+GeisAnn@users.noreply.github.com>
2021-03-27 18:31:21 +0300

    Проверка количества до и после

--- addressbook-web-tests/src/test/java/ru/aqa/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/aqa/addressbook/tests/GroupCreationTests.java
@@ -1,6 +1,7 @@
 package ru.aqa.addressbook.tests;
 
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.aqa.addressbook.model.GroupData;
 
@@ -10,12 +11,15 @@ public class GroupCreationTests extends TestBase {
   @Test
   public void testGroupCreationTests() throws Exception {
 
+
     app.getNavigationHelper().goToGroupPage();
+    int before = app.getGroupHelper().getGroupCount();
     app.getGroupHelper().initGroupCreation();
     app.getGroupHelper().fillGroupForm(new GroupData("test1", null, null));
     app.getGroupHelper().submitGroupCreation();
     app.getGroupHelper().returnToGroupPage();
-
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before +1);
   }
 }
 
--- addressbook-web-tests/src/test/java/ru/aqa/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/aqa/addressbook/tests/GroupModificationTests.java
@@ -1,5 +1,6 @@
 package ru.aqa.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.aqa.addressbook.model.GroupData;
 
@@ -8,16 +9,19 @@ public class GroupModificationTests extends TestBase {
   @Test
   public void testGroupModification() {
 
+
     app.getNavigationHelper().goToGroupPage();
     if (!app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test1", null, null));
     }
-
+    int before = app.getGroupHelper().getGroupCount();
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before);
 
   }
 }
