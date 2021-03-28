07f8afadcb35015de9165c8448eaaf20ab338137
GeisAnn <78374113+GeisAnn@users.noreply.github.com>
2021-03-28 18:08:21 +0300

    Сравнение множеств

--- addressbook-web-tests/src/test/java/ru/aqa/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/aqa/addressbook/model/GroupData.java
@@ -4,10 +4,19 @@ import java.util.Objects;
 
 public class GroupData {
   private final String name;
+  private final String id;
   private final String header;
   private final String footer;
 
+  public GroupData(String id, String name, String header, String footer) {
+    this.id = id;
+    this.name = name;
+    this.header = header;
+    this.footer = footer;
+  }
+
   public GroupData(String name, String header, String footer) {
+    this.id = null;
     this.name = name;
     this.header = header;
     this.footer = footer;
@@ -16,6 +25,8 @@ public class GroupData {
   public String getName() {
     return name;
   }
+
+  public String getId() { return id; }
 
   public String getHeader() {
     return header;
@@ -26,22 +37,24 @@ public class GroupData {
   }
 
   @Override
-  public String toString() {
-    return "GroupData{" +
-            "name='" + name + '\'' +
-            '}';
-  }
-
-  @Override
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     GroupData groupData = (GroupData) o;
-    return Objects.equals(name, groupData.name);
+    return Objects.equals(name, groupData.name) && Objects.equals(id, groupData.id);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(name);
+    return Objects.hash(name, id);
   }
+
+  @Override
+  public String toString() {
+    return "GroupData{" +
+            "name='" + name + '\'' +
+            ", id='" + id + '\'' +
+            '}';
+  }
+
 }
