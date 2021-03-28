d8a8991ccf0df29323ba494cccd6332db7f8f680
GeisAnn <78374113+GeisAnn@users.noreply.github.com>
2021-03-28 16:44:25 +0300

    Сравнение размера списка

--- addressbook-web-tests/src/test/java/ru/aqa/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/aqa/addressbook/tests/GroupCreationTests.java
@@ -5,6 +5,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.aqa.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupCreationTests extends TestBase {
 
 
@@ -13,13 +15,15 @@ public class GroupCreationTests extends TestBase {
 
 
     app.getNavigationHelper().goToGroupPage();
-    int before = app.getGroupHelper().getGroupCount();
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+
     app.getGroupHelper().initGroupCreation();
     app.getGroupHelper().fillGroupForm(new GroupData("test1", null, null));
     app.getGroupHelper().submitGroupCreation();
     app.getGroupHelper().returnToGroupPage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before +1);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+
+    Assert.assertEquals(after.size(), before.size() +1);
   }
 }
 
