bffa75652d79e4faffb6c4a23365edf08f2e431d
GeisAnn <78374113+GeisAnn@users.noreply.github.com>
2021-04-01 17:03:27 +0300

    Поиск максимального элемента

--- addressbook-web-tests/src/test/java/ru/aqa/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/aqa/addressbook/model/GroupData.java
@@ -4,11 +4,14 @@ import java.util.Objects;
 
 public class GroupData {
   private final String name;
-  private final String id;
+
+
+
+  private int id;
   private final String header;
   private final String footer;
 
-  public GroupData(String id, String name, String header, String footer) {
+  public GroupData(int id, String name, String header, String footer) {
     this.id = id;
     this.name = name;
     this.header = header;
@@ -16,17 +19,23 @@ public class GroupData {
   }
 
   public GroupData(String name, String header, String footer) {
-    this.id = null;
+    this.id = 0;
     this.name = name;
     this.header = header;
     this.footer = footer;
+  }
+
+
+
+  public void setId(int id) {
+    this.id = id;
   }
 
   public String getName() {
     return name;
   }
 
-  public String getId() { return id; }
+  public int getId() { return id; }
 
   public String getHeader() {
     return header;
@@ -37,24 +46,22 @@ public class GroupData {
   }
 
   @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    GroupData groupData = (GroupData) o;
-    return Objects.equals(name, groupData.name) && Objects.equals(id, groupData.id);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(name, id);
-  }
-
-  @Override
   public String toString() {
     return "GroupData{" +
             "name='" + name + '\'' +
             ", id='" + id + '\'' +
             '}';
   }
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    return id == groupData.id && Objects.equals(name, groupData.name);
+  }
 
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, id);
+  }
 }
